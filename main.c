994676122559b01518ccd05d4eff49619f19d19a
CorderoPonce <127542303+CorderoPonce@users.noreply.github.com>
2023-05-18 21:28:51 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -23,6 +23,67 @@ void mostrarMenu(){
   puts(BARRA);
 }
 
+bool validarPrioridad(char *prioridadTemp){
+  
+  int largoCadena = strlen(prioridadTemp);
+  if (largoCadena == 0) return false;
+  for (int i = 0 ; i < largoCadena ; i++){
+    if (isalpha(prioridadTemp[i])) return false;
+  }
+  return true;
+}
+
+int validarNombre(List *l, HashMap *m, char *nombre) {
+  for (char *a = first(l) ; a != NULL ; a = next(l)) {
+    if (strcmp(a, nombre) == 0) return 1;
+  }
+  
+  int largoName = strlen(nombre); 
+  if (largoName > 31 || largoName < 1) {
+    return 2;
+  }
+  return 0;
+}
+
+void agregarTarea(List *l, HashMap *m){
+  char nombre[31];
+  puts("Ingrese el nombre de la tarea");
+  scanf(" %[^\n]", nombre);
+
+  int validacion;
+  while (validacion != 0){
+    validacion = validarNombre(l, m, nombre);
+    
+    if (validacion == 1) {
+      puts("El nombre ingresado ya existe. Intente nuevamente");
+      scanf(" %[^\n]", nombre);
+    }
+    if (validacion == 2) {
+      puts("Ingrese un nombre válido (hasta 30 caracteres)");
+      scanf(" %[^\n]", nombre);
+    }
+  }
+
+  char prioridadTemp[31];
+  puts("Ingrese la prioridad de la tarea");
+  scanf(" %[^\n]", prioridadTemp);
+
+  while (validarPrioridad(prioridadTemp) == false){
+    puts("Se debe ingresar un número");
+    scanf(" %[^\n]", prioridadTemp);
+  }
+  int prioridad = atoi(prioridadTemp);
+  
+  Tarea *n = (Tarea *) malloc (sizeof(Tarea));
+  strcpy(n->nombre, nombre);
+  n->prioridad = prioridad;
+  n->numPrecedentes = 0;
+  n->precedencia = createList();
+
+  insertMap(m, n->nombre, n);
+  //ListaEnOrden(l, m, n);
+}
+
 int main(){
   int num;
   List *l = createList();
@@ -49,7 +110,7 @@ int main(){
       return 0;
     }
     if (num == 1){
-      //agregarTarea(l, m);
+      agregarTarea(l, m);
       puts("* Tarea agregada con éxito");
     }
     if (num == 2){
